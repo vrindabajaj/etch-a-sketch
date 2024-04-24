4ace23c97b9370a0f98c59ca1b90a4e031877274
vrindabajaj <78031928+vrindabajaj@users.noreply.github.com>
2024-04-24 10:09:25 +0100

    Add grid resizing function

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const grid = document.querySelector(".grid");
-const square = document.createElement("div");
 
+function calculateSquareSize(size){
+    const square = document.createElement("div");
+    square.classList.toggle("square");
+    square.style.width = `${grid.clientWidth/size}px`;
+    square.style.height = `${grid.clientHeight/size}px`;
+    grid.appendChild(square);
+}
 
 function createGrid(size){
     for (let i = 0; i < size; i++){
         for (let j = 0; j < size; j++){
-            const square = document.createElement("div");
-            square.classList.toggle("square");
-            grid.appendChild(square);
+            calculateSquareSize(size);
         }
     }
 }
